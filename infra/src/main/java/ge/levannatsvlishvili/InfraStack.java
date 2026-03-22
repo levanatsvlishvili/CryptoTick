6d3439637b659f8c40f4e502bf01a115c89fc10f
wero <levani28100@gmail.com>
2026-03-22 20:40:35 +0400

    feat: add lambda function in cdk

--- infra/src/main/java/ge/levannatsvlishvili/InfraStack.java
+++ infra/src/main/java/ge/levannatsvlishvili/InfraStack.java
@@ -5,9 +5,14 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.cognito.*;
 import software.amazon.awscdk.services.dynamodb.*;
+import software.amazon.awscdk.services.lambda.*;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.events.*;
+import software.amazon.awscdk.services.events.targets.LambdaFunction;
 import software.amazon.awscdk.Duration;
 import software.constructs.Construct;
+import java.util.Map;
 
 public class InfraStack extends Stack {
     public InfraStack(final Construct scope, final String id, final StackProps props) {
@@ -17,7 +22,6 @@ public class InfraStack extends Stack {
                 .queueName("CryptoTick_Ingestion_Queue")
                 .visibilityTimeout(Duration.seconds(300))
                 .build();
-
 
         Table alertsTable = Table.Builder.create(this, "CryptoAlertsTable")
                 .tableName("CryptoTick_Alerts")
@@ -35,9 +39,24 @@ public class InfraStack extends Stack {
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
 
-
         userPool.addClient("CryptoTickWebClient", UserPoolClientOptions.builder()
                 .authFlows(AuthFlow.builder().userPassword(true).build())
                 .build());
+
+        Function ingestionLambda = Function.Builder.create(this, "IngestionLambda")
+                .runtime(Runtime.JAVA_17)
+                .handler("ge.levannatsvlishvili.IngestionHandler")
+                .code(Code.fromAsset("../services/ingestion-service/target/ingestion-service-1.0-SNAPSHOT.jar"))
+                .timeout(Duration.seconds(30))
+                .memorySize(512)
+                .environment(Map.of("QUEUE_URL", ingestionQueue.getQueueUrl()))
+                .build();
+
+        ingestionQueue.grantSendMessages(ingestionLambda);
+
+        Rule eventRule = Rule.Builder.create(this, "IngestionSchedule")
+                .schedule(Schedule.expression("rate(1 minute)"))
+                .build();
+        eventRule.addTarget(new LambdaFunction(ingestionLambda));
     }
 }
